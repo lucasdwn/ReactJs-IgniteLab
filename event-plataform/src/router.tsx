dca3e6c00e0cb0a671836e2be08d3d9319017d18
lucasdwn <lfcosta0804@gmail.com>
2022-06-25 11:19:40 -0300

    aula 04

--- event-plataform/src/router.tsx
+++ event-plataform/src/router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Rout, Routes, Route } from "react-router-dom";
 import { Event } from "./pages/Event";
+import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
     return(
         <Rout>
             <Routes>
-                <Route path="/" element={<h1>Home</h1>}/>
+                <Route path="/" element={<Subscribe/>}/>
                 <Route path="/event" element={<Event />}/>
                 <Route path="/event/lesson/:slug" element={<Event />}/>
             </Routes>
